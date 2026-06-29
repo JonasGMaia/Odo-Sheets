12192ab80e9f915c9893f3a2d24b74ad12fe1bb5
Jonas Maia <jonasgmaia2@gmail.com>
2026-06-29 16:30:57 -0300

    ligando headless browser

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
     const hospedagem = pendentes[0];
     console.log(`\n🌐 Novo link encontrado! Acessando: ${hospedagem.url}`);
 
-    const browser = await chromium.launch({ headless: false }); 
+    const browser = await chromium.launch({ headless: true }); 
     const context = await browser.newContext();
     const page = await context.newPage();
 
